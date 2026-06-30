611203c2c6d1d1145e80b4b76219d337cdbda8d3
dnexe007 <235278071+dnexe007@users.noreply.github.com>
2026-06-30 11:27:54 +0300

    Update COM script

---
+++ Assets/Scripts/Setup/COM.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COM : MonoBehaviour
+{
+    [SerializeField] private Vector3 COMLocalPosition;
+    private Rigidbody rb;
+
+	private void Start() => UpdateCOM();
+    private void OnValidate() => UpdateCOM();
+
+	private void UpdateCOM()
+	{
+		if (rb == null) rb = GetComponent<Rigidbody>();
+		if (rb != null) rb.centerOfMass = COMLocalPosition;
+	}
+
+	private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + transform.TransformDirection(COMLocalPosition), 1);
+    }
+}

51b289037eae1663fe629dd31d70de96816987d9
dnexe007 <235278071+dnexe007@users.noreply.github.com>
2026-07-06 23:49:18 +0300

    MovementDrag with floating rotating factor

--- Assets/Scripts/Aerodynamics/Body/MovementDrag.cs
+++ Assets/Scripts/Aerodynamics/Body/MovementDrag.cs
@@ -4,9 +4,7 @@ public class MovementDrag : MonoBehaviour
 {
     private Rigidbody rb;
     private AircraftSetup setup;
-
     MovementDragConfig config => setup.config.MovementDragConfig;
-    //Vector3 ForcePoint => rb.worldCenterOfMass + rb.transform.TransformDirection(config.forcePointOffset);
 
     private void Start()
     {
@@ -16,15 +14,15 @@ public class MovementDrag : MonoBehaviour
 
     private void FixedUpdate()
     {
+        float angleOfAttack = Vector3.Angle(rb.velocity, transform.forward);
 
-       // float angleMult = config.resistanceMultOverFlowAngle.Evaluate(Vector3.Angle(transform.forward, rb.velocity));
-        //float basicForce = config.resistanceAnchor.GetDrag(rb.velocity.magnitude);
+        Vector3 dragVector = config.GetDragVector(rb.velocity, angleOfAttack);
+        float rotatingFactor = config.GetRotatingFactor(angleOfAttack);
 
-        //Vector3 forceVector = -rb.velocity.normalized * basicForce * angleMult;
-
-        var drag = config.GetFuselageDrag(rb.velocity, transform.forward, rb.worldCenterOfMass);
-        rb.AddForceAtPosition(drag.force, drag.position, ForceMode.Force);
-
-       // rb.AddForceAtPosition(forceVector, ForcePoint, ForceMode.Acceleration);
+        rb.AddForceAtPosition(
+            dragVector,
+            rb.worldCenterOfMass + rb.transform.forward * rotatingFactor,
+            ForceMode.Force
+        );
     }
 }
--- Assets/Scripts/Config/MovementDragConfig.cs
+++ Assets/Scripts/Config/MovementDragConfig.cs
@@ -10,24 +10,43 @@ public class MovementDragConfig
 		public Vector3 position;
 	}
 
-	[SerializeField]
-	private AnimationCurve resistanceMultOverFlowAngle = new(
+	[SerializeField] private AnimationCurve dragMultOverAOA = new(
 		new(0f, 1f, 0.1f, 0.1f),
 		new(90f, 10f, 0f, 0f),
 		new(180f, 1f, -0.1f, -0.1f)
 	);
 
-	[SerializeField] private QuadDragAnchor resistanceAnchor = new(500, 200_000);
+	[SerializeField] private AnimationCurve rotatingFactorMultOverAOA = new(
+		new(0, 1),
+		new(90, 0),
+		new(180, -1)
+	);
+
+	[SerializeField] private QuadDragAnchor dragAnchor = new(500, 200_000);
+
 	[SerializeField] private float rotatingFactor = -0.5f;
 
 	public DragData GetFuselageDrag(Vector3 velocity, Vector3 forward, Vector3 worldCOM)
 	{
-		float basicDrag = resistanceAnchor.GetDrag(velocity.magnitude);
-		float dragMult = resistanceMultOverFlowAngle.Evaluate(Vector3.Angle(velocity, forward));
+		float basicDrag = dragAnchor.GetDrag(velocity.magnitude);
+		float dragMult = dragMultOverAOA.Evaluate(Vector3.Angle(velocity, forward));
 		Vector3 force = -velocity.normalized * basicDrag * dragMult;
 
 		Vector3 position = worldCOM + forward * rotatingFactor;
 
 		return new() { force = force, position = position };
+	}
+
+	public Vector3 GetDragVector(Vector3 velocity, float angleOfAttack)
+	{
+		float basicDrag = dragAnchor.GetDrag(velocity.magnitude);
+		float angleMult = dragMultOverAOA.Evaluate(angleOfAttack);
+		return -velocity.normalized * basicDrag * angleMult;
+	}
+
+	public float GetRotatingFactor(float angleOfAttack)
+	{
+		float angleMult = rotatingFactorMultOverAOA.Evaluate(angleOfAttack);
+		return rotatingFactor * angleMult;
 	}
 }
